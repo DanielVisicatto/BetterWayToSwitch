84096ddfbf9055b17e5455c7a25c8f30da796ce5
Daniel Visicatto <dpfreestyle@gmail.com>
2023-06-16 09:59:34 -0300

    Add project files.

---
+++ BetterWayToSwitch/Program.cs
@@ -0,0 +1,15 @@
+﻿var today = DateTime.UtcNow;
+
+string output = today.DayOfWeek switch
+{
+    DayOfWeek.Monday =>     "é segunda-feira",
+    DayOfWeek.Tuesday =>    "é terça-feira",
+    DayOfWeek.Wednesday =>  "é quarta-feira",
+    DayOfWeek.Thursday =>   "é quinta-feira",
+    DayOfWeek.Friday =>     "sextou!!!",
+    DayOfWeek.Saturday =>   "é sabadão",
+    DayOfWeek.Sunday =>     "to triste, amanhã é segunda =X",
+                    _ =>    "Ta malucão?"
+};
+
+Console.WriteLine($"Hoje {output}");
