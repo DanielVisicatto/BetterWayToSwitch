965a5fa7c10fc9c3f35aa0cd700556a42d0af939
Daniel Visicatto <dpfreestyle@gmail.com>
2023-06-16 10:02:47 -0300

    Identação

--- BetterWayToSwitch/Program.cs
+++ BetterWayToSwitch/Program.cs
@@ -2,14 +2,14 @@
 
 string output = today.DayOfWeek switch
 {
-    DayOfWeek.Monday =>     "é segunda-feira",
-    DayOfWeek.Tuesday =>    "é terça-feira",
-    DayOfWeek.Wednesday =>  "é quarta-feira",
-    DayOfWeek.Thursday =>   "é quinta-feira",
-    DayOfWeek.Friday =>     "sextou!!!",
-    DayOfWeek.Saturday =>   "é sabadão",
-    DayOfWeek.Sunday =>     "to triste, amanhã é segunda =X",
-                    _ =>    "Ta malucão?"
+    DayOfWeek.Monday        =>      "é segunda-feira",
+    DayOfWeek.Tuesday       =>      "é terça-feira",
+    DayOfWeek.Wednesday     =>      "é quarta-feira",
+    DayOfWeek.Thursday      =>      "é quinta-feira",
+    DayOfWeek.Friday        =>      "sextou!!!",
+    DayOfWeek.Saturday      =>      "é sabadão",
+    DayOfWeek.Sunday        =>      "to triste, amanhã é segunda =X",
+                          _ =>      "Ta malucão?"
 };
 
 Console.WriteLine($"Hoje {output}");
